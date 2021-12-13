e394654de8154f87511182ca8fd1f9201cebaa81
ryoAccount <jane.doe3wtp4649@gmail.com>
2021-12-14 05:52:19 +0900

    get object

--- Assets/PlayerHP.cs
+++ Assets/PlayerHP.cs
@@ -5,12 +5,16 @@ using UnityEngine.UI;
 
 public class PlayerHP : MonoBehaviour
 {
+  public GameObject hpGameObject;
   // Start is called before the first frame update
   void Start()
   {
-    Text HPText = GetComponent<Text>();
-    HPText.text = "100";
-    HPText.color = new Color(1f, 0f, 0f, 1f);
+    // Text HPText = GetComponent<Text>();
+    // HPText.text = "100";
+    // HPText.color = new Color(1f, 0f, 0f, 1f);
+
+    Text text = hpGameObject.GetComponent<Text>();
+    text.text = "50";
   }
 
   // Update is called once per frame
