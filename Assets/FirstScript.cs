5b89d8d6cf334ff6b847009ad3c9de75062f2c97
ryoAccount <jane.doe3wtp4649@gmail.com>
2022-01-11 05:17:24 +0900

    switch

--- Assets/FirstScript.cs
+++ Assets/FirstScript.cs
@@ -10,12 +10,29 @@ public class FirstScript : MonoBehaviour
     string say = "Hello world";
     string name = "rm";
     // const string say = "Hello world const";
-    Debug.Log(say + ":" + name);
-    Debug.Log(string.Format("{0}:{1}", say, name));
+    Debug.Log(say + ":" + name); // Hello world:rm
+    Debug.Log(string.Format("{0}:{1}", say, name)); // Hello world:rm
 
     int x = 100;
     string y = "200";
-    Debug.Log(x + y);
+    Debug.Log(x + y); // 100200
+
+    string characterType = "player";
+    switch (characterType)
+    {
+      case "player":
+        Debug.Log("P");
+        break;
+      case "enemy":
+        Debug.Log("E");
+        break;
+      case "friend":
+        Debug.Log("F");
+        break;
+      default:
+        Debug.Log("D");
+        break;
+    }
 
   }
 
