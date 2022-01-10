9c57b499d6bbd228f96b12e112ace0a0e9fe11e7
ryoAccount <jane.doe3wtp4649@gmail.com>
2022-01-11 05:38:58 +0900

    if

--- Assets/FirstScript.cs
+++ Assets/FirstScript.cs
@@ -7,33 +7,52 @@ public class FirstScript : MonoBehaviour
   // Start is called before the first frame update
   void Start()
   {
-    string say = "Hello world";
-    string name = "rm";
-    // const string say = "Hello world const";
-    Debug.Log(say + ":" + name); // Hello world:rm
-    Debug.Log(string.Format("{0}:{1}", say, name)); // Hello world:rm
+    // string say = "Hello world";
+    // string name = "rm";
+    // // const string say = "Hello world const";
+    // Debug.Log(say + ":" + name); // Hello world:rm
+    // Debug.Log(string.Format("{0}:{1}", say, name)); // Hello world:rm
 
-    int x = 100;
-    string y = "200";
-    Debug.Log(x + y); // 100200
+    // int x = 100;
+    // string y = "200";
+    // Debug.Log(x + y); // 100200
 
-    string characterType = "player";
-    switch (characterType)
+    // string characterType = "player";
+    // switch (characterType)
+    // {
+    //   case "player":
+    //     Debug.Log("P");
+    //     break;
+    //   case "enemy":
+    //     Debug.Log("E");
+    //     break;
+    //   case "friend":
+    //     Debug.Log("F");
+    //     break;
+    //   default:
+    //     Debug.Log("D");
+    //     break;
+    // }
+
+    int x = 0;
+    while (x <= 10)
     {
-      case "player":
-        Debug.Log("P");
+      if (x == 3)
+      {
+        x++;
+        continue;
+      }
+      if (x > 7)
+      {
         break;
-      case "enemy":
-        Debug.Log("E");
-        break;
-      case "friend":
-        Debug.Log("F");
-        break;
-      default:
-        Debug.Log("D");
-        break;
+      }
+      Debug.Log(x);
+      x++;
     }
-
+    // for (int y = 0; y <= 10; y++)
+    // {
+    //   Debug.Log(y);
+    // }
   }
 
   // Update is called once per frame
