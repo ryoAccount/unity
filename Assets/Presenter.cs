f4a457494e003be7ad4f9e59380122aaf3de88f2
ryoAccount <jane.doe3wtp4649@gmail.com>
2022-01-12 06:54:17 +0900

    component | object

--- Assets/Presenter.cs
+++ Assets/Presenter.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Presenter : MonoBehaviour
 {
+  // [SerializeField] TestScript testScript;
+  [SerializeField] Text testText;
   public void OnClickButton()
   {
     Debug.Log("click");
-    SceneManager.LoadScene("Main");
+    // SceneManager.LoadScene("Main");
+    // image.SetActive(false);
+    // Destroy(image);
+    // testScript.TestText();
+    testText.text = "aaaaaaaaaaaa";
   }
 }
