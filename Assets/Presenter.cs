61b2f30ae3aa974e0e978850e6d2c14ae1361f7e
ryoAccount <jane.doe3wtp4649@gmail.com>
2022-01-13 23:37:46 +0900

    save | json

--- Assets/Presenter.cs
+++ Assets/Presenter.cs
@@ -1,19 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Presenter : MonoBehaviour
 {
-  [SerializeField] Transform parent;
-  [SerializeField] GameObject imagePrefab;
   void Start()
   {
-    // create object
-    GameObject image = Instantiate(imagePrefab);
+    // save
+    // string x = "Save Data";
+    // PlayerPrefs.SetString("keyword", x);
+    // PlayerPrefs.Save();
 
-    // parent
-    image.transform.SetParent(parent, false);
+    // load
+    // string text = PlayerPrefs.GetString("keyword");
+    // Debug.Log(text);
+
+    // json
+    PlayerModel player = new PlayerModel();
+    string jsonPlayer = JsonUtility.ToJson(player);
+    Debug.Log(jsonPlayer);
+
+    // json -> object
+    player = JsonUtility.FromJson<PlayerModel>(jsonPlayer);
   }
+}
+
+[SerializeField]
+public class PlayerModel
+{
+  [SerializeField]
+  int hp;
+  [SerializeField]
+  int at;
+  [SerializeField]
+  int curretStage;
 }
