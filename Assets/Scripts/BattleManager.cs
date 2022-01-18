50864bcaec8d445f527daa8846793a20686c478c
ryoAccount <jane.doe3wtp4649@gmail.com>
2022-01-18 23:34:07 +0900

    setup

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -11,9 +11,24 @@ public class BattleManager : MonoBehaviour
   // Start is called before the first frame update
   void Start()
   {
+    Setup();
+  }
+
+  // initialize
+  void Setup()
+  {
+    enemyUI.SetupUI(enemy);
+    playerUI.SetupUI(player);
+  }
+
+  void AttackPlayer()
+  {
     player.Attack(enemy);
     enemyUI.UpdateUI(enemy);
+  }
 
+  void AttackEnemy()
+  {
     enemy.Attack(player);
     playerUI.UpdateUI(player);
   }
