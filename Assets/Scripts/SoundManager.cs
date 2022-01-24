a0781ea5ca5eee9c741df937633181b4b06c9b39
ryoAccount <jane.doe3wtp4649@gmail.com>
2022-01-25 00:33:26 +0900

    scene bgm

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -20,15 +20,46 @@ public class SoundManager : MonoBehaviour
       Destroy(this.gameObject);
     }
   }
-  public AudioSource audioSourceSE; // speaker
-  public AudioClip audioClip; // sound
+
+  public AudioSource audioSourceBGM;
+  public AudioClip[] audioClipsBGM; // 0:Title, 1:Town, 2:Quest, 3:Battle
+
+  public AudioSource audioSourceSE;
+  public AudioClip audioClip;
 
   void Start()
   {
+    // PlayBGM("Title");
     // audioSourceSE.PlayOneShot(audioClip);
   }
 
-  public void playSE()
+  public void PlayBGM(string sceneName)
+  {
+    audioSourceBGM.Stop();
+
+    switch (sceneName)
+    {
+      case "Title":
+        audioSourceBGM.clip = audioClipsBGM[0];
+        break;
+      case "Town":
+        audioSourceBGM.clip = audioClipsBGM[1];
+        break;
+      case "Quest":
+        audioSourceBGM.clip = audioClipsBGM[2];
+        break;
+      case "Battle":
+        audioSourceBGM.clip = audioClipsBGM[3];
+        break;
+      default:
+        audioSourceBGM.clip = audioClipsBGM[0];
+        break;
+    }
+
+    audioSourceBGM.Play();
+  }
+
+  public void PlaySE()
   {
     audioSourceSE.PlayOneShot(audioClip); // play se
   }
